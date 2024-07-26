0a77801c2209565cde78226026fd9807c9ea1c16
zhill0950 <zhill0950@gmail.com>
2024-07-26 01:01:13 -0700

    add questions variable

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+import fs from "fs";
+import inquirer from "inquirer";
 
+const questions = [
+  {
+    name: "title",
+    message: "What is the title of your project?",
+  },
+
+  {
+    name: "description",
+    message: "Please provide a description of your project.",
+  },
+
+  {
+    name: "installation",
+    message: "Please provide installation instructions for your project.",
+  },
+
+  {
+    name: "usage",
+    message: "Please provide usage information for your project.",
+  },
+
+  {
+    name: "license",
+    message: "Please provide license information for your project.",
+  },
+
+  {
+    name: "contributing",
+    message: "Please provide contributing information for your project.",
+  },
+
+  {
+    name: "tests",
+    message: "Please provide test instructions for your project.",
+  },
+
+  {
+    name: "questions",
+    message: "Please provide question information for your project.",
+  },
+
+  {
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+
+  {
+    name: "email",
+    message: "What is your email address?",
+  },
+];
