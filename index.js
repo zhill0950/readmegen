8be4a2f68bb82a87e61356cd6245586b15662cb5
zhill0950 <zhill0950@gmail.com>
2024-07-26 01:02:35 -0700

    create template literal for readme file generation

--- index.js
+++ index.js
@@ -52,3 +52,46 @@ const questions = [
     message: "What is your email address?",
   },
 ];
+
+function genReadMe(data) {
+  return `# ${data.title}
+    
+    ## Description
+    ${data.description}
+    
+    ## Table of Contents
+    - [Installation](#installation)
+    
+    - [Usage](#usage)
+    
+    - [License](#license)
+    
+    - [Contributing](#contributing)
+    
+    - [Tests](#tests)
+    
+    - [Questions](#questions)
+    
+    ## Installation
+    ${data.installation}
+    
+    ## Usage
+    ${data.usage}
+    
+    ## License
+    ${data.license}
+    
+    ## Contributing
+    ${data.contributing}
+    
+    ## Tests
+    ${data.tests}
+    
+    ## Questions
+    ${data.questions}
+    
+    ## Contact
+    - GitHub: [${data.username}]
+    
+    - Email: ${data.email}`;
+}
